98d9f79f18de75e2508a24bab0cc5b87f84b9926
Jomar Quiban <104065461+JoQuibs@users.noreply.github.com>
2023-01-30 18:20:24 -0800

    Added Formula
    Revised the codes

--- temp_conversion.php
+++ temp_conversion.php
@@ -1,4 +1,3 @@
-<!DOCTYPE HTML>
 <html>
 
 <head>
@@ -8,20 +7,16 @@
     <title>Temperature Converter</title>
     <link rel="stylesheet" href="css/styles.css" type="text/css">
 </head>
-  
-  <body>
+
+<body>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
         <h2>Temperature Converter</h2>
-      
-      <fieldset>
-        <label>Temperature to Convert</label>
-            <input type="number" 
-                   name="temp_from" 
-                   step="any" 
-                   value="<?php if (isset($_POST['temp_from'])) echo htmlspecialchars($_POST['temp_from']); ?>" 
-            />
-        
-        <label>Convert From:</label>
+
+        <fieldset>
+            <label>Temperature to Convert</label>
+            <input type="number" name="temp_from" step="any" value="<?php if (isset($_POST['temp_from'])) echo htmlspecialchars($_POST['temp_from']); ?>" />
+
+            <label>Convert From:</label>
             <select name="scale_from">
 
                 <option value="" NULL<?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == NULL) echo 'selected = "unselected"'; ?>> Select one</option>
@@ -33,152 +28,76 @@
                 <option value="k" <?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == 'k') echo 'selected = "selected"'; ?>> Kelvin </option>
 
             </select>
-        
-        
-        
-        
-      </fieldset>
-      
-      
+
+            <label>Convert To:</label>
+            <select name="scale_to">
+                <option value="" NULL<?php if (isset($_POST['scale_to']) && $_POST['scale_from'] == NULL) echo 'selected = "unselected"'; ?>>Select one</option>
+
+                <option value="c" <?php if (isset($_POST['scale_to']) && $_POST['scale_to'] == 'c') echo 'selected = "selected"'; ?>>Celsius</option>
+
+                <option value="k" <?php if (isset($_POST['scale_to']) && $_POST['scale_to'] == 'k') echo 'selected = "selected"'; ?>>Kelvin</option>
+
+                <option value="f" <?php if (isset($_POST['scale_to']) && $_POST['scale_to'] == 'f') echo 'selected = "selected"'; ?>>Fahrenheit</option>
+            </select>
+
+            <a href="" class="reset">Reset</a>
+        </fieldset>
+        <input type="submit" />
     </form>
-    
-<?php
-//initalize variables
-$input = '';
-$conversion = '';
-$msg = '';
 
-function fahrToCel($arg)
-{
-    $num = $arg;
-    $int_val = intval($num);
-    $conversion = (($int_val - 32) * 5) / 9;
-    return $conversion;
-}
+    <?
+    //form.php
 
-function fahrToKel($arg)
-{
-    $num = $arg;
-    $int_val = intval($num);
-    $conversion = (($int_val - 32) * 5) / 9 + 273.15;
-    return $conversion;
-}
+    // header('Content-Type: text/html; charset=utf-8');
 
-function celToKel($arg)
-{
-    $num = $arg;
-    $int_val = intval($num);
-    $conversion = $int_val + 273.15;
-    return $conversion;
-}
+    echo '';
+    if (isset($_POST["temp_from"])) { // show data
 
-function celToFahr($arg)
-{
-    $num = $arg;
-    $int_val = intval($num);
-    $conversion = ($int_val * 9) / 5 + 32;
-    return $conversion;
-}
+        $temp_from = $_POST["temp_from"];
+        $scale_from = $_POST["scale_from"];
+        $scale_to = $_POST["scale_to"];
+        // $result = 0;
 
-function kelToFahr($arg)
-{
-    $num = $arg;
-    $int_val = intval($num);
-    $conversion = (($int_val - 273.15) * 9) / 5 + 32;
-    return $conversion;
-}
+        if (!is_numeric($temp_from)) {
+            echo "<p class='error'>Temperature must be number.</p>";
+        }
 
-function kelToCel($arg)
-{
-    $num = $arg;
-    $int_val = intval($num);
-    $conversion = $int_val - 273.15;
-    return $conversion;
-}
-
-function conversionMsg($input, $conversion)
-{
-    return '<p class = "result">The conversion of <b>' .
-        $input .
-        '</b> from <b>' .
-        strtoupper($_POST['tempA'][0]) .
-        '°</b> to <b>' .
-        strtoupper($_POST['tempB'][0]) .
-        '°</b> is equal to <b>' .
-        number_format((float) $conversion, 2) .
-        '</b> degrees!</p>';
-}
-
-#=======================================================================
-//check server request
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //Check POST associate array
-    if (empty($_POST['userInput'])) {
-        $msg = '<p class="error">input a value</p>';
-    } else {
-        $input = intval($_POST['userInput']);
-
-        //if both toggle empty, print warning
-        if (
-            !array_key_exists('tempA', $_POST) &&
-            !array_key_exists('tempB', $_POST)
-        ) {
-            $msg =
-                '<p class="error">Please pick two temperatures to convert</p>';
+        if (($scale_from == "") || ($scale_to == "")) {
+            echo "<p class='error'>Please select both temperature scales.</p>";
+        } elseif ($scale_from == $scale_to) {
+            echo "<p class='error'>Temperature scales must be different.</p>";
+        } elseif ($scale_from == "f" && $scale_to == "c") {
+            $result = ($temp_from - 32) * (5 / 9);
+        } elseif ($scale_from == "c" && $scale_to == "f") {
+            $result = ($temp_from * (9 / 5)) + 32;
+        } elseif ($scale_from == "c" && $scale_to == "k") {
+            $result = $temp_from + 273.15;
+        } elseif ($scale_from == "k" && $scale_to == "c") {
+            $result = $temp_from - 273.15;
+        } elseif ($scale_from == "f" && $scale_to == "k") {
+            $result = ($temp_from + 459.67) * (5 / 9);
+        } elseif ($scale_from == "k" && $scale_to == "f") {
+            $result = ($temp_from * (9 / 5)) - 459.67;
         } else {
-            //keys exists
-            //if one is toggled and not the other, print warning
-            if (isset($_POST['tempA']) && !isset($_POST['tempB'])) {
-                $msg = '<p class="error">Please pick a conversion type</p>';
-            } elseif (!isset($_POST['tempA']) && isset($_POST['tempB'])) {
-                $msg =
-                    '<p class="error">Please pick a temperature to convert</p>';
-            } else {
-                //temp combo options
-                if ($_POST['tempA'] == 'fahr' && $_POST['tempB'] == 'cel') {
-                    $conversion = fahrToCel($input);
-                    $msg = conversionMsg($input, $conversion);
-                } elseif (
-                    $_POST['tempA'] == 'fahr' &&
-                    $_POST['tempB'] == 'kel'
-                ) {
-                    $conversion = fahrToKel($input);
-                    $msg = conversionMsg($input, $conversion);
-                } elseif (
-                    $_POST['tempA'] == 'cel' &&
-                    $_POST['tempB'] == 'fahr'
-                ) {
-                    $conversion = celToFahr($input);
-                    $msg = conversionMsg($input, $conversion);
-                } elseif (
-                    $_POST['tempA'] == 'cel' &&
-                    $_POST['tempB'] == 'kel'
-                ) {
-                    $conversion = celToKel($input);
-                    $msg = conversionMsg($input, $conversion);
-                } elseif (
-                    $_POST['tempA'] == 'kel' &&
-                    $_POST['tempB'] == 'fahr'
-                ) {
-                    $conversion = kelToFahr($input);
-                    $msg = conversionMsg($input, $conversion);
-                } elseif (
-                    $_POST['tempA'] == 'kel' &&
-                    $_POST['tempB'] == 'cel'
-                ) {
-                    $conversion = kelToCel($input);
-                    $msg = conversionMsg($input, $conversion);
-                } else {
-                    // temp same, please try again
-                    $msg = conversionMsg($input, $input);
-                }
-            }
+            echo "<p class='error'>Please complete entire form.</p>";
+        }
+
+        if (
+            is_numeric($temp_from) &&
+            is_numeric($result) &&
+            !empty($scale_from) &&
+            !empty($scale_to)
+        ) {
+            echo '
+            <div class="box">
+            <p>' . $temp_from . '&#176' . strtoupper($scale_from) . ' = ' . round($result, 2) . '&#176' . strtoupper($scale_to) . '</p>
+            </div>
+          ';
         }
     }
-}
 
-//end server request
-?>
-    
-  </body>
+    ?>
+
+</body>
+
 </html>
